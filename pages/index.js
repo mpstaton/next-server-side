2e97bcea767214d588fa41e78b84ac0d5c926c4f
Michael Staton <mpstaton@gmail.com>
2020-09-20 07:36:49 -0700

    added redux and dependencies, set up index file to tap redux, created Actions folder and file

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Link from 'next/link';
-import GetLink from "../SharedComponents/DynamicRouter";
-import "../style.css";
-
-function MyComponent(){
+import '../style.css';
+export default class extends React.Component {
+  static async getInitialProps() {
+    return { text: 'Initial label value.' }
+  }
+  constructor(props) {
+    super(props);
+    this.state = { text: props.text };
+  }
+  render() {
     return(
-        <div>
-        <p>Hello from Next.js!</p>
-            <Link href='/About'>
-                <a>About</a>
-            </Link>
-
-            <p>A GetLink attempt</p>
-            <GetLink title='Page 1' 
-                Disp='page-1'
-                Class="img p1"></GetLink>
-            <GetLink title='Page 2' 
-                Disp='page-2'
-                Class="img p2"></GetLink>
-            <GetLink title='Page 3' 
-                Disp='page-3'
-                Class="img p3"></GetLink>        
+      <div>
+        <h1>Redux Demo</h1>
+        <br />
+        <div className="center">
+          <input id="inputTextbox" type="text">
+          </input>
+          <button type="button"
+              onClick={this.GetUser}>
+              Update Label
+          </button>
         </div>
+        <br />
+        <p>{this.state.text}</p>
+      </div>
     );
+  }
 }
-
-export default MyComponent;
