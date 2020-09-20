bec8f15ba89dfffc7e19fdcb37a41b1b7c0fa6ba
Michael Staton <mpstaton@gmail.com>
2020-09-20 08:32:25 -0700

    explained redux a little more

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import '../style.css';
-export default class extends React.Component {
-  static async getInitialProps() {
-    return { text: 'Initial label value.' }
-  }
-  constructor(props) {
-    super(props);
-    this.state = { text: props.text };
-  }
+import "../style.css";
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeState } from '../Actions/actions'
+class ReduxDemo extends React.Component {
   render() {
-    return(
+    return (
       <div>
         <h1>Redux Demo</h1>
         <br />
@@ -17,13 +13,25 @@ export default class extends React.Component {
           <input id="inputTextbox" type="text">
           </input>
           <button type="button"
-              onClick={this.GetUser}>
+              onClick={this.props.changeState}>
               Update Label
           </button>
         </div>
         <br />
-        <p>{this.state.text}</p>
+        <p>{this.props.text}</p>
       </div>
     );
   }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    changeState: bindActionCreators(changeState, dispatch)
+  }
+}
+/*props is now injected by the react-redux library. 
+The first parameter contains State-related entities that you want to inject to the Page, and the second parameter is the Page itself. 
+We are injecting the text property that resides in the state object. We are also injecting the method that is used to dispatch an Action to modify the Store object. 
+We pass this method to the click event of the button by accessing it in the Page using “this.props.changeState”. 
+We also bind the label (<p>) to the “text” property of State object using “this.props.text”.
+*/
+export default connect((state) => ({ text: state.text }), mapDispatchToProps)(ReduxDemo)
